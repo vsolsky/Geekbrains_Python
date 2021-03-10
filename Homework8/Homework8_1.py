3fc79877204db20ace0df3831d150033e7d182fb
Vadym <vadim.solsky@gmail.com>
2021-03-10 07:10:32 +0100

    Task 1-3

--- Homework8/Homework8_1.py
+++ Homework8/Homework8_1.py
@@ -3,5 +3,30 @@
 # год и преобразовывать их тип к типу «Число». Второй, с декоратором @staticmethod, должен проводить валидацию числа,
 # месяца и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 
+class Date:
+    @staticmethod
+    def date_validation(date):
+        day, month, year = Date.date_parsing(date)
+        if day<0 or day>31:
+            print(f"Error your day is not within the range, should be within 0-31, yours is {day} ")
+            exit(1)
+        elif month<0 or month>12:
+            print(f"Error your month is not within the range, should be within 0-12, yours is {month} ")
+            exit(1)
+        elif year<0 or year>2100:
+            print(f"Error your year is not within the range, should be within 0-2100, yours is {year} ")
+            exit(1)
+        else:
+            print("Your date is in correct format")
+    @classmethod
+    def date_parsing (cls, date):
+        cls.date=date
+        day, month, year= cls.date.split('-')
+        day=int(day)
+        month=int(month)
+        year=int(year)
+        return day,month, year
 
+date="30-12-2020"
+Date.date_validation(date)
 
